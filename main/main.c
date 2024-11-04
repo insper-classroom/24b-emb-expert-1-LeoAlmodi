eef584b4931fb5df359be3186ea96c1551579e9d
LeoAlmodi <leonardoar2@al.insper.edu.br>
2024-11-04 14:06:17 -0300

    feito mas o audio ta horrivel

--- main/main.c
+++ main/main.c
@@ -1,17 +1,56 @@
 /**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ * Copyright (c) 2021 Raspberry Pi (Trading) Ltd.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
 #include <stdio.h>
-#include "pico/stdlib.h"
+#include "pico/stdlib.h"   // stdlib 
+#include "hardware/irq.h"  // interrupts
+#include "hardware/pwm.h"  // pwm 
+#include "hardware/sync.h" // wait for interrupt 
 #include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include "hardware/uart.h"
+#include "pico/binary_info.h"
+ 
+// Audio PIN is to match some of the design guide shields. 
+#define AUDIO_PIN 28  // you can change this to whatever you like
+#define ADC_NUM 0
+#define ADC_PIN (26 + ADC_NUM)
+#define ADC_VREF 3.3
+#define ADC_RANGE (1 << 12)
+#define ADC_CONVERT (ADC_VREF / (ADC_RANGE - 1))
+#define BTN_PIN 15
+
 
 int main() {
     stdio_init_all();
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    adc_init();
+    adc_gpio_init( ADC_PIN);
+    adc_select_input( ADC_NUM);
+    gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
+
+    int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
+
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 8.0f); 
+    pwm_config_set_wrap(&config, 250); 
+    pwm_init(audio_pin_slice, &config, true);
+
+    pwm_set_gpio_level(AUDIO_PIN, 0);
+    uint16_t adc_raw;
+
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
+
+    while (1) {
+        if (!gpio_get(BTN_PIN)) {
+            adc_raw = adc_read(); // raw voltage from ADC
+            pwm_set_gpio_level(AUDIO_PIN, adc_raw);
+        } else {
+            pwm_set_gpio_level(AUDIO_PIN, 0);
+        }
+        
+        sleep_us(100);
     }
 }
